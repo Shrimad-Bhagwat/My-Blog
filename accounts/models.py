7f21e83c97e0b3268c2c38ff324127f7442a0d6f
Shrimad Bhagwat <51125208+Shrimad-Bhagwat@users.noreply.github.com>
2021-08-12 22:41:27 +0530

    Add files via upload

--- accounts/models.py
+++ accounts/models.py
@@ -3,15 +3,13 @@ from django.db import models
 # Create your models here.
 class Feedback(models.Model):
     Uname = models.CharField(max_length=50)
-    feedback = models.TextField(max_length=300)
-
-    
-    
+    feedback = models.TextField()
     def __str__(self):
         return "Message from " + self.Uname
 
 class Post(models.Model):
     heading = models.CharField(max_length=50)
-    content = models.TextField(max_length=1000)
+    content = models.TextField()
+    img = models.ImageField(upload_to='postImages')
     def __str__(self):
         return "Post : " + self.heading
