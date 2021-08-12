cb728fefa01a19cd6fe2fd30b7716167dcc1119e
Shrimad Bhagwat <51125208+Shrimad-Bhagwat@users.noreply.github.com>
2021-08-12 22:43:14 +0530

    Add files via upload

--- UserProject/urls.py
+++ UserProject/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +27,5 @@ urlpatterns = [
 admin.site.site_header = "SB Portal"
 admin.site.site_title = "SB"
 admin.site.index_title = "Welcome to SB Portal"
+
+urlpatterns += static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)
